b81fe1a4f1de8b3d10816601d6f04d7a71dc0ba5
Valentin Escoyez <valentin.escoyez@gmail.com>
2015-04-16 14:02:37 +0200

    git ignore node_modules & jspm_packages

--- config.js
+++ config.js
@@ -17,6 +17,8 @@ System.config({
     "bootstrap": "github:twbs/bootstrap@3.3.4",
     "css": "github:systemjs/plugin-css@0.1.9",
     "font-awesome": "npm:font-awesome@4.3.0",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
     "github:aurelia/binding@0.5.0": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.6.0",
       "aurelia-metadata": "github:aurelia/metadata@0.4.0",
