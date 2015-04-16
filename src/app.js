68c9721f610d22e5a7a53cacf1fe64ae22fedcff
Valentin Escoyez <valentin.escoyez@gmail.com>
2015-04-16 15:18:13 +0200

    several artists/featured artists/remix case

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 export class TrackMeta{
-  track = 'Mark Ronson feat. Bruno Mars - Uptown Funk (Radio Edit)';
+  track = `Bill Withers & Grover Washington feat. test1 & test2 - Just the Two of Us (feat. test3)[radio remix]`;
 
   get trackMeta(){
-    return parse(this.track);
+    return parseTrack(this.track);
   }
 
   getTrackMeta(){
-    alert( parse(this.track) );
+    alert( parseTrack(this.track) );
   }
 }
 
-function parse(track){
+function parseTrack(track){
   var spl = track.split(/([\(\[\)\]]|\s-\s|feat\.|ft\.)/);
-  var res = {};
-  res.mainArtists = spl.shift();
+  var res = {
+    mainArtists: [],
+    featuredArtists: [],
+    songRemix: []
+  };
+  res.mainArtists = res.mainArtists.concat( spl.shift().trim().split(` & `) );
   for(var i = 0; i < spl.length; ++i)
   {
     switch(spl[i].toLowerCase()){
@@ -24,9 +28,9 @@ function parse(track){
       case "[" :
       case "]" : break;
       case "feat." :
-      case "ft." : res.featuredArtists = spl[++i].trim(); break;
+      case "ft." : res.featuredArtists = res.featuredArtists.concat( spl[++i].trim().split(` & `) ); break;
       case " - " : res.songTitle = spl[++i].trim(); break;
-      default : res.songRemix = spl[i].trim();
+      default : res.songRemix = res.songRemix.concat( spl[i].trim().split(` & `) );
     }
   }
   return res;
