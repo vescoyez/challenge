d55a3a05fa86d1d7ffa5ee66d766dbfe9d47c9bf
Valentin Escoyez <valentin.escoyez@gmail.com>
2015-04-16 14:39:00 +0200

    Track parsing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 export class TrackMeta{
   track = 'Mark Ronson feat. Bruno Mars - Uptown Funk (Radio Edit)';
 
-  get mainArtists(){
-    return this.track;
+  get trackMeta(){
+    return parse(this.track);
   }
 
-  trackMeta(){
-    alert(this.mainArtists);
+  getTrackMeta(){
+    alert( parse(this.track) );
   }
+}
+
+function parse(track){
+  var spl = track.split(/([\(\[\)\]]|\s-\s|feat\.|ft\.)/);
+  var res = {};
+  res.mainArtists = spl.shift();
+  for(var i = 0; i < spl.length; ++i)
+  {
+    switch(spl[i].toLowerCase()){
+      case "" :
+      case " " :
+      case "(" :
+      case ")" :
+      case "[" :
+      case "]" : break;
+      case "feat." :
+      case "ft." : res.featuredArtists = spl[++i].trim(); break;
+      case " - " : res.songTitle = spl[++i].trim(); break;
+      default : res.songRemix = spl[i].trim();
+    }
+  }
+  return res;
 }
